110bfd0158f3ee678617e1f08266a7dbc5e56748
tkallaus <tkallaus@asu.edu>
2018-04-15 21:31:41 -0700

    After some deliberation, added code from mobile branch to this one manually.

--- HygieaRecycle/App.js
+++ HygieaRecycle/App.js
@@ -23,19 +23,26 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  
+  
+  
   render() {
     return (
       <View style={styles.container}>
+
+        <Image source={require('HygieaRecycle/assets/logo.png')} style={{width: 300, height: 200}}/>
+        <Text>v0.0.0.0001</Text>
+
         <Camera
    ref={(cam) => {
        this.camera = cam;
     }}
     style={styles.preview}
     aspect={Camera.constants.Aspect.fill}>
-       <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
-          [CAPTURE]
-       </Text>
-</Camera>
+        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+           [CAPTURE]
+        </Text>
+      </Camera>
 
       </View>
     );
@@ -82,3 +89,35 @@ const styles = StyleSheet.create({
     margin: 40
  }
 });
+
+//Basic "send a request and ask for a result"-type function
+//returns the response as a json and also prints it to the console.
+function askForResult() {
+    return fetch('https://www.ourBackEndServer.com/', {
+      credentials: 'omit'
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+      });
+}
+
+//Sends a file to the fetch location, and prints out a response in the form of a json
+//Hoping to test these functions out more once there's an actual photo to send and an actual server to send to.
+function sendPhoto() {
+  var form = new FormData();
+  var fileField = document.querySelector("input[type='file']"); //Picture?
+  
+  //form.append(username, password);
+  form.append('picture', fileField.files[0]);
+
+  fetch('https://www.ourBackendServer.com/postData', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response));
+}
