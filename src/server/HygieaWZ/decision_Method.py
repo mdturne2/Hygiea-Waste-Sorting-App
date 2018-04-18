72dfc321f156fc8964701203f5eac7d488eec9e4
Joshua Albin <jaalbin@asu.edu>
2018-04-18 14:01:12 -0700

    Task #18 Finished Desicison Module
    Completed the basic version of the decision module.

--- src/server/HygieaWZ/decision_Method.py
+++ src/server/HygieaWZ/decision_Method.py
@@ -1,28 +1,27 @@
 ﻿#Decision Making Method for which BIN the ITEM should be placed into.
 
-#
-#import (stuff from API: Json file documentation, Database of words used for association, and exactly the return type [still need])
-#
-
 #----------VARIABLES USED IN THIS MODULE-----------#
 
-#counter
-c = 0 
-
-#length for how many label annotations there are
-len = len(labelAnnotations)
+#array of recycle words
+recycle = ['plastic', 'aluminum', 'bottle', 'recycle', 'recyclable', 'paper']
 
 #--------------------------------------------------#
 
-#List for descriptions for the ITEM
-decrip = ['']
+#function for decision
+def decision (arg):
 
-#Adding all the descriptions from label annotations into a list
-while c < len:
-    decrip.append(labelAnnotations[c][2])
-    c = c + 1
+    #if the word from Json is the one that needs to be read is there
+    if (type(arg) == type(list)):
 
-#NOTE FOR JOSH
-# Figure out some way to remove the part: "description:" "WORD" 
-# in order to simplify the process....
+        #goes through the array of possible words for recycle
+        for i in recycle:
+
+            #if one of them is in the list of words from the image,
+            if (arg in recycle[i]): 
+
+                #tell the program that the item needs to go to the recycle bin
+                return 'Recycle'
+
+        #if not, tell the program that the item needs to go to the compost bin
+        return 'compost'
 
